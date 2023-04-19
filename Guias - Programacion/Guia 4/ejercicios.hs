4769c35bd681087a56678abd0683f916492a3963
Manuel <manuel.torrado@gmail.com>
2023-04-19 17:52:15 -0300

    Ejercicio 5

--- Guias - Programacion/Guia 4/ejercicios.hs
+++ Guias - Programacion/Guia 4/ejercicios.hs
@@ -1,14 +1,34 @@
-fibonacci:: Integer->Integer
+fibonacci :: Integer->Integer
 fibonacci n | n == 0 = 0
 	          | n == 1 = 1
 					  | otherwise = fibonacci(n-1) + fibonacci(n-2) 
 
-parteEntera:: Float -> Integer
+parteEntera :: Float -> Integer
 parteEntera x | x >= 0 && x < 1 = 0    
 	            | otherwise  =  1 + parteEntera(x-1) 
 
-esDivisible:: Integer -> Integer-> Bool
+esDivisible :: Integer -> Integer-> Bool
 esDivisible x y | x < y = False
-	              | (x==y) = True
-                | otherwise = esDivisible (x-y) (y)
-							  
+							  | (x==y) = True
+							  | otherwise = esDivisible (x-y) (y)
+
+-- Funcion auxiliar, te devuelve el enesimo impar que le pidas
+imparNumero :: Integer -> Integer 
+imparNumero x | x == 1 = 1
+              | otherwise = 2 + imparNumero(x-1)
+
+sumaImpares :: Integer -> Integer 
+sumaImpares x | x == 1 = 1 
+	            | otherwise = imparNumero(x) + sumaImpares(x-1)
+
+
+-- Ejercicio 5
+factorial :: Integer -> Integer 
+factorial x | x == 0 = 1
+					  | otherwise = factorial(x-1) * x
+
+medioFact :: Integer -> Integer
+medioFact x | x == 0 = 1
+				    | x == 1 = 1
+					  | otherwise = x * medioFact(x-2)
+          	
