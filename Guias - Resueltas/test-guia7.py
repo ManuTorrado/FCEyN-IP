a24b020d6a45a458075c89fc69fbd298edf2399e
ManuTorrado <manuel.torrado@gmail.com>
2023-06-10 20:06:24 -0300

    Casos de test guia 7

--- Guias - Resueltas/test-guia7.py
+++ Guias - Resueltas/test-guia7.py
@@ -1,5 +1,12 @@
-from tester import test
+from tester import test, multipleTest
 import guia7
 
 test(guia7.raizDe2, 1.4142)
+print("-------------------------------------------------------")
 test(guia7.factorial_de_2, 2)
+print("-------------------------------------------------------")
+multipleTest(guia7.factorial_de, [
+             1, 1, 2, 6, 24, 120, 720, 5040, 40320], [0, 1, 2, 3, 4, 5, 6, 7, 8])
+print("-------------------------------------------------------")
+multipleTest(guia7.raiz_cuadrada_de, [
+             0.0, 1.0, 2.0, 4.0, 5.0], [0, 1, 4, 16, 25])
--- Guias - Resueltas/tester.py
+++ Guias - Resueltas/tester.py
@@ -1,6 +1,20 @@
 # En desarrollo
+
+# Se introduce la funcion, el resultado que esperamos y los argumentos de la funcion
 def test(program, expected: any, *args) -> bool:
     res: bool = program(*args) == expected
     print('Esperado: ', expected)
     print('Salida: ', program(*args))
-    print(res)
+    return res
+
+
+# Sirve para hacer varios tests en una sola funcion
+def multipleTest(program, cases: list[any], args: list[any]) -> bool:
+    res: bool = True
+    aux: int = 0
+    for case in cases:
+        if (test(program, case, args[aux]) == False):
+            res = False
+            print('Test fails')
+        aux += 1
+    return res
